a82e0013c100be9a76baa1d05fd42579fa3b94c1
fixgramwork <kimminje661@outlook.kr>
2024-11-26 20:10:56 +0900

    1302 김민재

--- src/User.java
+++ src/User.java
@@ -1,3 +1,29 @@
+import java.time.LocalDate;
+
 public class User {
 
+  private Long id;
+  private String email;
+  private String name;
+  private String password;
+  private String createAt;
+
+  public User(String email, String name, String password) {
+    this.email = email;
+    this.name = name;
+    this.password = password;
+    this.createAt = LocalDate.now().toString();
+  }
+
+
+  public Long getId() {return id;}
+  public String getEmail() {return email;}
+  public String getName() {return name;}
+  public String getPassword() {return password;}
+  public String getCreateAt() {return createAt;}
+
+  public void setId(Long id) {this.id = id;}
+  public void setEmail(String email) {this.email = email;}
+  public void setName(String name) {this.name = name;}
+  public void setPassword(String password) {this.password = password;}
 }
--- src/UserService.java
+++ src/UserService.java
@@ -1,2 +1,38 @@
-public class UserService {
+import java.util.*;
+
+public class UserService implements CrudService<User> {
+  Map<Long, User> users = new HashMap<>();
+  private Long nextId = 1L;
+
+  @Override
+  public User create(User entity) {
+    entity.setId(nextId++);
+    users.put(entity.getId(), entity);
+    return entity;
+  }
+
+  @Override
+  public User read(Long id) {
+    return users.get(id);
+  }
+
+  @Override
+  public User update(Long id, User entity) {
+    if (users.containsKey(id)) {
+      entity.setId(id);
+      users.put(id, entity);
+      return entity;
+    }
+    return null;
+  }
+
+  @Override
+  public boolean delete(Long id) {
+    return users.remove(id) != null;
+  }
+
+  @Override
+  public List<User> listAll() {
+    return new ArrayList<>(users.values());
+  }
 }
